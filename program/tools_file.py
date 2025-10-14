b0b8187c77ac4402bb552ae65f15524349355ed6
LE2-edg <edgarlefauconniermarini@gmail.com>
2025-10-14 16:07:17 +0200

    code pin définis

--- program/tools_file.py
+++ program/tools_file.py
@@ -4,20 +4,14 @@ import json
 with open("ressource_folder/data.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
-#La fonction sld permet de retourner le solde du client
-def sld ():
-    for account in data:
-        return account["sold"]
+def reading_file ():
+    pass
 
-#La foction old_withdrawal permet de retourner les anciens retraits du client
-def old_withdrawal ():
-    for account in data:
-        return account["withdrawal"]
-    
-def new_sold ():
-    if n > 0:
-        sld = sld + n
-    if n < 0:
-        sld = sld + n 
-    if n == 0:
-        sld = sld + 0
+def extract ():
+    pass
+
+def written ():
+    pass
+
+def correct_pin ():
+    pass
