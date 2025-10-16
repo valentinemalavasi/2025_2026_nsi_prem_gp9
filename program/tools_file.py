d72233c9ccc97030a3c0ec2277203acdaf460ca9
LE2-edg <edgarlefauconniermarini@gmail.com>
2025-10-16 12:01:53 +0200

    Update tools_file.py

--- program/tools_file.py
+++ program/tools_file.py
@@ -4,14 +4,27 @@ import json
 with open("ressource_folder/data.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
-def reading_file ():
+def system (account):
     pass
 
-def extract ():
+def account (data):
     pass
+
+def reading_file ():
+    for e in data:
+        account in range (len)
 
 def written ():
-    pass
+    for e in system:
+        pass
+
+def answer ():
+    for answer in input():
+        pass
 
 def correct_pin ():
-    pass
+    for pin_c in account:
+        if answer == pin:
+            return True
+        else:
+            return False
