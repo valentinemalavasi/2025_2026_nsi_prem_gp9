e07ea0e4357e369f56bd837237ccea3fa013219f
valentinemalavasi <valentine.malavasi@gmail.com>
2025-11-15 12:17:06 +0100

    Update tools_file.py

--- program/tools_file.py
+++ program/tools_file.py
@@ -1,4 +1,3 @@
-#importation du fichier data.json dans le dossier ressource
 import json
 
 def load_data_from_json (json_file):
@@ -9,28 +8,3 @@ def load_data_from_json (json_file):
 def write_in_json_file (data, json_file):
     with open (json_file, "w", encoding="utf-8") as f:
         json.dump (data, f, indent=4)
-
-def system (account):
-    pass
-
-def account (data):
-    pass
-
-def reading_file ():
-    for e in data:
-        account in range (len)
-
-def written ():
-    for e in system:
-        pass
-
-def answer ():
-    for answer in input():
-        pass
-
-def correct_pin ():
-    for pin_c in account:
-        if answer == pin:
-            return True
-        else:
-            return False
