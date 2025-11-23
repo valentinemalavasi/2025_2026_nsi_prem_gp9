a0b3883d3813c6ee23f922fc4cea6273d7628df4
KIRBY54 <giulio92100@gmail.com>
2025-11-23 13:09:13 +0100

    Update tools_file.py

--- program/tools_file.py
+++ program/tools_file.py
@@ -1,29 +1,20 @@
+# tools_file.py
+
+"""
+import json
 """
 import json
 
-ressource_folder = "ressources/"
-
 def load_data_from_json (json_file):
-    with open (json_file in ressource_folder, "r") as f:
+    """Charge les données depuis un fichier JSON."""
+   
+    with open (json_file, "r") as f:
         data = json.load(f)
     return data
 
 def write_in_json_file (data, json_file):
-    with open (json_file in ressource_folder, "w") as f:
+    """Écrit les données dans un fichier JSON avec une indentation de 4."""
+   
+    with open (json_file, "w") as f:
         json.dump (data, f, indent=4)
 
-import json
-"""
-import data.json
-ressource_folder = "ressource_folder/"
-json_file = "data.json/"
-
-def load_data_from_json(data.json_file):
-    with open(ressource_folder + json_file, "r") as f:
-        data = data.json.load(f)
-    return data
-
-
-def write_in_json_file(data, json_file):
-    with open(ressource_folder + json_file, "w") as f:
-        data.json.dump(data, f, indent=4)
